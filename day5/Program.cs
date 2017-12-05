ace4de14fd34c8648fb4bfc7b9338ef79541f8d6
Simon Nattress <nattress@gmail.com>
2017-12-04 21:11:52 -0800

    Day 5

---
+++ day5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> instructions = new List<int>();
+
+            using (TextReader tr = File.OpenText(args[0]))
+            {
+                while(true)
+                {
+                    string line = tr.ReadLine();
+                    if (string.IsNullOrEmpty(line))
+                        break;
+                    instructions.Add(int.Parse(line));
+                }
+            }
+
+            int stepCount = 0;
+            int position = 0;
+            while (true)
+            {
+                stepCount++;
+                int numToJump = instructions[position];
+
+                if (numToJump >= 3)
+                {
+                    instructions[position] -= 1;    
+                }
+                else
+                    instructions[position]++;
+
+                position += numToJump;
+
+                if (position >= instructions.Count)
+                {
+                    Console.WriteLine($"{stepCount} jumps.");
+                    return;
+                }
+            }
+
+        }
+    }
+}
