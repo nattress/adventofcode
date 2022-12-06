c9742c0ccddfe427d9f6f3bbd63e865365cfd8ee
Simon Nattress <nattress@gmail.com>
2022-12-05 18:12:52 -0800

    Parts 3, 4

---
+++ 2022/03/Program.cs
@@ -0,0 +1,92 @@
+﻿public class AdventOfCode
+{
+    private static string GetCommonCharacter(string ruckSack)
+    {
+        HashSet<string> left = new HashSet<string>();
+        HashSet<string> right = new HashSet<string>();
+
+        for (int i = 0; i < ruckSack.Length / 2; i++)
+        {
+            left.Add(ruckSack[i].ToString());
+            right.Add(ruckSack[ruckSack.Length / 2 + i].ToString());
+        }
+
+        return left.Intersect(right).First();
+    }
+
+    private static char GetCommonCharacter(string sack1, string sack2, string sack3)
+    {
+        return sack1.Intersect(sack2).Intersect(sack3).First();
+    }
+
+    public static void Main(string[] args)
+    {
+        List<string> commonCharacters = new List<string>();
+        List<string> sackGroups = new List<string>();
+        List<char> sackGroupCommonCharacters = new List<char>();
+
+        using (TextReader tr = File.OpenText(args[0]))
+        {
+            while (true)
+            {
+                string? line = tr.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    break;
+
+                commonCharacters.Add(GetCommonCharacter(line));
+
+                sackGroups.Add(line);
+                
+                if (sackGroups.Count == 3)
+                {
+                    sackGroupCommonCharacters.Add(GetCommonCharacter(sackGroups[0], sackGroups[1], sackGroups[2]));
+                    sackGroups.Clear();
+                }
+            }
+
+            int part1 = Part1(commonCharacters);
+            int part2 = Part2(sackGroupCommonCharacters);
+
+            Console.WriteLine($"Part One: {part1}.");
+            Console.WriteLine($"Part Two: {part2}.");
+        }
+    }
+
+    static int Part1(List<string> commonCharacters)
+    {
+        int result = 0;
+
+        foreach (string s in commonCharacters)
+        {
+            if (char.IsUpper(s[0]))
+            {
+                result += s[0] - 'A' + 27;
+            }
+            else
+            {
+                result += s[0] - 'a' + 1;
+            }
+        }
+
+        return result;
+    }
+
+    static int Part2(List<char> commonCharacters)
+    {
+        int result = 0;
+
+        foreach (char c in commonCharacters)
+        {
+            if (char.IsUpper(c))
+            {
+                result += c - 'A' + 27;
+            }
+            else
+            {
+                result += c - 'a' + 1;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ 2022/04/Program.cs
@@ -0,0 +1,50 @@
+﻿public class AdventOfCode
+{
+    static bool RangeIsSubset(int oneStart, int oneEnd, int twoStart, int twoEnd)
+    {
+        return oneStart >= twoStart && oneEnd <= twoEnd ||
+            twoStart >= oneStart && twoEnd <= oneEnd;
+    }
+
+    static bool RangeOverlaps(int oneStart, int oneEnd, int twoStart, int twoEnd)
+    {
+        return oneStart >= twoStart && oneStart <= twoEnd ||
+            twoStart >= oneStart && twoStart <= oneEnd;
+    }
+
+    public static void Main(string[] args)
+    {
+        // 1. Add state tracking for file parsing 
+        int partOneOverlapCount = 0;
+        int partTwoOverlapCount = 0;
+        using (TextReader tr = File.OpenText(args[0]))
+        {
+            while (true)
+            {
+                string? line = tr.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    break;
+
+                int[] elfPair = line.Split(new char[] {',', '-'}).Select((item, index) => int.Parse(item)).ToArray();
+                if (RangeIsSubset(elfPair[0], elfPair[1], elfPair[2], elfPair[3]))
+                    partOneOverlapCount++;
+                
+                if (RangeOverlaps(elfPair[0], elfPair[1], elfPair[2], elfPair[3]))
+                    partTwoOverlapCount++;
+            }
+
+            Console.WriteLine($"Part One: {partOneOverlapCount}.");
+            Console.WriteLine($"Part Two: {partTwoOverlapCount}.");
+        }
+    }
+
+    static int Part1()
+    {
+        return 0;
+    }
+
+    static int Part2()
+    {
+        return 0;
+    }
+}
