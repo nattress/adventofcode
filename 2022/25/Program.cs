12253756bd9d6696d552a3a08b7d744221fb16d6
Simon Nattress <nattress@gmail.com>
2022-12-05 21:08:53 -0800

    Create all the project files

---
+++ 2022/25/Program.cs
@@ -0,0 +1,35 @@
+﻿public class AdventOfCode
+{
+    public static void Main(string[] args)
+    {
+        // 1. Add state tracking for file parsing 
+
+        using (TextReader tr = File.OpenText(args[0]))
+        {
+            while (true)
+            {
+                string? line = tr.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    break;
+
+                // 2. Add logic for per-line handling
+            }
+
+            int part1 = Part1();
+            int part2 = Part2();
+
+            Console.WriteLine($"Part One: {part1}.");
+            Console.WriteLine($"Part Two: {part2}.");
+        }
+    }
+
+    static int Part1()
+    {
+        return 0;
+    }
+
+    static int Part2()
+    {
+        return 0;
+    }
+}
---
+++ 2022/scripts/create-project-files.ps1
@@ -0,0 +1,23 @@
+#
+# Sets up the folder structure and project files for Advent Of Code 2022
+#
+
+$currentYearSourceRoot = $PSScriptRoot | Split-Path -Parent
+
+Write-Output "Creating projects under $currentYearSourceRoot"
+
+for ($i = 1; $i -lt 26; $i++)
+{
+    $dayFolder = $i.ToString().PadLeft(2, '0');
+    $daySourceRoot = Join-Path -Path $currentYearSourceRoot -ChildPath $dayFolder
+    Write-Output $daySourceRoot
+
+    New-Item -Path $daySourceRoot -ItemType Directory
+    Set-Location $daySourceRoot
+    dotnet new console
+
+    $templateProgram = Join-Path -Path $currentYearSourceRoot -ChildPath "Program.cs"
+    Copy-Item -Path $templateProgram -Destination $daySourceRoot -Force
+
+    Set-Location $PSScriptRoot
+}
