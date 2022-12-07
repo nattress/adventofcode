1e6400416cd79649eb2d2757cfa9d45a5a1f7be6
Simon Nattress <nattress@gmail.com>
2022-12-06 21:56:42 -0800

    Day 6

--- 2022/06/Program.cs
+++ 2022/06/Program.cs
@@ -4,29 +4,33 @@
     {
         using TextReader tr = File.OpenText("input_01.txt");
         
-        while (true)
-        {
-            string? line = tr.ReadLine();
-            if (string.IsNullOrEmpty(line))
-                break;
-            
-            // Todo: Parse input.
-        }
-
-        int part1 = Part1();
-        int part2 = Part2();
+        string? line = tr.ReadLine();
+        int part1 = FindUniqueCharacterRun(line!, 4);
+        int part2 = FindUniqueCharacterRun(line!, 14);
 
         Console.WriteLine($"Part One: {part1}.");
         Console.WriteLine($"Part Two: {part2}.");
     }
 
-    static int Part1()
+    static int FindUniqueCharacterRun(string input, int runLength)
     {
-        return 0;
-    }
+        HashSet<char> letters = new HashSet<char>();
+        for (int i = 0; i < input.Length - runLength; i++)
+        {
+            for (int j = 0; j < runLength; j++)
+            {
+                letters.Add(input[i + j]);
+            }
+            
+            if (letters.Count == runLength)
+            {
+                // done
+                return i + runLength;
+            }
 
-    static int Part2()
-    {
-        return 0;
+            letters.Clear();
+        }
+
+        throw new InvalidDataException();
     }
 }
