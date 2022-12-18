7a60379ce8547d4f3bbe57b2a16799d5dffd20b8
Simon Nattress <nattress@gmail.com>
2022-12-18 14:12:50 -0800

    WIP

--- 2022/08/Program.cs
+++ 2022/08/Program.cs
@@ -4,25 +4,47 @@
     {
         using TextReader tr = File.OpenText("input_01.txt");
         
+        List<int[]> grid = new List<int[]>();
+
         while (true)
         {
             string? line = tr.ReadLine();
             if (string.IsNullOrEmpty(line))
                 break;
             
-            // Todo: Parse input.
+            int[] newRow = new int[line.Length];
+            int column = 0;
+            foreach (char c in line)
+            {
+                int treeHeight = int.Parse(c.ToString());
+                newRow[column++] = treeHeight;
+            }
+
+            grid.Add(newRow);
         }
 
-        int part1 = Part1();
+        int part1 = CountVisible(grid);
         int part2 = Part2();
 
         Console.WriteLine($"Part One: {part1}.");
         Console.WriteLine($"Part Two: {part2}.");
     }
 
-    static int Part1()
+    static int CountVisible(List<int[]> grid)
     {
-        return 0;
+        Dictionary<(int, int), bool> visibleMap = new Dictionary<(int, int), bool>();
+        int numberVisible = 0;
+        for (int y = 0; y < grid.Count; y++)
+        {
+            
+        }
+
+        for (int x = 0; x < grid[0].Length; x++)
+        {
+            
+        }
+
+        return numberVisible;
     }
 
     static int Part2()
